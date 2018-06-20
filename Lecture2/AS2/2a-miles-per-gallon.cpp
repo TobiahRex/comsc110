0818e8e0e0b27ec6fc0b7c70864bd02c65ee2a9a
Tobiah Rex <tobiah.rex@gmail.com>
2018-06-20 11:50:57 -0700

    writing 2b.

--- Lecture2/AS2/2a-miles-per-gallon.cpp
+++ Lecture2/AS2/2a-miles-per-gallon.cpp
@@ -2,9 +2,21 @@
 using namespace std;
 
 int main() {
-  float gas_tank;
+  float gas_tank, mileage, miles_per_gallon;
 
   cout << "Enter the size of the gas tank: " << endl;
-  cin >> gas_tank << endl;
+  cin >> gas_tank;
+  cout << "Enter the total mileage for a full tank of gas: " << endl;
+  cin >> mileage;
+
+  miles_per_gallon = mileage / gas_tank;
+
+  cout << "If a car holds"
+  << gas_tank << " gallons of gasoline and can travel "
+  << mileage << " miles before refueling, then it will go about "
+  << miles_per_gallon << " MPG."
+  << endl;
+
+
   return 0;
 }
---
+++ Lecture2/AS2/2b-stadium-seating.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  cout << "Enter the number of Class A tickets sold: " << endl;``
+  return 0;
+}
