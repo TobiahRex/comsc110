45fd42eb15d6f586d883ce771479f4dcad8f86b5
Tobiah Rex <tobiah.rex@gmail.com>
2018-06-24 18:08:23 -0700

    added variables to track length of the number.

--- AS2/2d-math-tutor.cpp
+++ AS2/2d-math-tutor.cpp
@@ -13,16 +13,22 @@
 using namespace std;
 
 int main() {
-  int rand1, rand2, solution;
+  int rand1, rand2, solution, r1Length, r2Length;
   srand(time(NULL));
 
   rand1 = rand() % 1000;
   rand2 = rand() % 1000;
   solution = rand1 + rand2;
-  string lineBreak(8, "_")
+  r1Length = to_string(rand1).length();
+  r2Length = to_string(rand2).length();
 
-  cout << printf("%8d", rand1) << endl;
-  cout << "+" << printf("%3d \n", rand2) << rand2 << endl;
+  string lineBreak(8, '_');
+
+  cout << "rand1: " << rand1 << endl;
+  cout << "rand2: " << rand2 << endl << endl;
+
+  printf ("%*d \n", 8, rand1);
+  printf ("+ %*d \n", 7, rand2);
   cout << lineBreak << endl;
   cout << '\n';
   cout << "(Press Enter To See Answer)" << endl;
