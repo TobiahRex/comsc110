c6e243502833c8f4f678a37b075d711907baf163
Tobiah Rex <tobiah.rex@gmail.com>
2018-07-02 18:29:08 -0700

    rendering results of 4d to console.

--- AS4/4d-distance-travelled.cpp
+++ AS4/4d-distance-travelled.cpp
@@ -5,3 +5,52 @@
 /// Date: July 2, 2018
 /// Status : Complete
 ///***********************************************************************************
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+  string number_error("\nInput must be a valid number, no letters.  Please try again.\n");
+
+  cout << "What is the speed of the vehicle in mph? ";
+
+  bool input{true};
+  int speed{};
+
+  while (input) {
+    cin >> speed;
+    if (speed == 0) {
+      cout << number_error << endl;
+      cin.clear();
+      cin.ignore();
+    } else {
+      input = false;
+    }
+  }
+
+  cout << "For how many hours has it traveled? ";
+
+  int hrs_travel{};
+  input = true;
+  while (input) {
+    cin >> hrs_travel;
+    if (hrs_travel == 0) {
+      cout << number_error << endl;
+      cin.clear();
+      cin.ignore();
+    } else {
+      input = false;
+    }
+  }
+
+  string results_title("\t Hour \tDistance \tTravelled (mi.)");
+  string linebreak(12, "_");
+  cout << results_title << endl << linebreak;
+
+  do {
+    cout << hrs_travel << endl;
+  } while(--hrs_travel > 0);
+
+  return 0;
+}
