63e18b007f829eee9f2ee0a325beae57a296c219
Tobiah Rex <tobiah.rex@gmail.com>
2018-07-03 09:50:04 -0700

    finished writing 5c.

--- AS5/5c-lowest-score.cpp
+++ AS5/5c-lowest-score.cpp
@@ -1,20 +1,24 @@
 ///**********************************************************************************
-/// Description: Calculates the falling distance of an object and displays the results.
+/// Description: Determines the average score by dropping the lowest of 5 total scores.
 /// Author: Tobiah Bickley
 /// COMSC 110 Section 1035
 /// Date: July 3, 2018
 /// Status : Complete
 ///***********************************************************************************
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 void getScore(int & s) {
-  cout << "Enter a test score: ";
+
   bool ask{true};
+
   while(ask) {
+    cout << "Enter a test score: ";
     cin >> s;
     if (s == 0) {
-      cout << "That is not a valid score.  Please try again";
+      cout << "That is not a valid score.  Please try again\n";
       cin.clear();
       cin.ignore();
     } else ask = false;
@@ -22,7 +26,19 @@ void getScore(int & s) {
 }
 
 int main() {
+  vector<int> scores = {0, 0, 0, 0, 0};
+  for (auto & score : scores) {
+    getScore(score);
+  }
 
+  sort(scores.begin(), scores.end());
+  int i{1};
+  double average{};
+  do {
+    average += scores[i];
+  } while(++i < 5);
+
+  cout << "\nAfter dropping the lowest score the new average is: " << (average / 4) << endl;
 
   return 0;
 }
