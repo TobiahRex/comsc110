a67e0948f1e4769c4d4f4113bc41859cd1e55fec
Tobiah Rex <tobiah.rex@gmail.com>
2018-07-05 20:25:50 -0700

    updated 6b.

--- AS6/6b-rainfall-stats.cpp
+++ AS6/6b-rainfall-stats.cpp
@@ -10,15 +10,18 @@
 #include <string>
 #include <algorithm>
 #include <iomanip>
+#include <limits>
 using namespace std;
 
 void printResults(
-  float const& s
-  float const& a
-  float const& l
+  float const& s,
+  float const& a,
+  float const& l,
+  float const& sm,
   float const& lgM,
-  float const& smM,
+  float const& smM
 ) {
+  cout << "smM: " << smM;
   cout << setprecision(2) << fixed;
   cout << "\nthe total rainfall for the year is " << s << " inches." << endl;
   cout << "the average rainfall for the year is " << a << " inches." << endl;
@@ -36,7 +39,7 @@ void getResults(vector<float> & r, float & sum, float & avg, float & lrg, float 
   sm = r[0];
 }
 
-void askForRain(vector<float> & n, int & month, float & lrgM, float & smM) {
+void askForRain(vector<float> & n, int & month, float & lrgM, float & smM, float & lstSm, float & lstLg) {
   string error("\nThat is not a valid input.  \nPlease enter a valid floating point number & try again\n");
 
   while(true) {
@@ -49,8 +52,17 @@ void askForRain(vector<float> & n, int & month, float & lrgM, float & smM) {
       cin.clear();
       cin.ignore();
     } else {
-      if (number > lrgM) lrgM = n;
-      else if (number < smM) smM = n;
+      if (number > lstLg) {
+        lrgM = month;
+        lstLg = number;
+      }
+      if (number < lstSm) {
+         smM = month;
+         lstSm = number;
+       }
+
+      // lrgM == 0 ? lrgM = 1 : smM == 0 ? smM = 1 : 1;
+
       n.push_back(number);
       break;
     }
@@ -58,11 +70,26 @@ void askForRain(vector<float> & n, int & month, float & lrgM, float & smM) {
 }
 
 void beginProgram() {
-  float sum{}, average{}, largest{}, smallest{};
-  int month{0}, largestMonth{}, smallestMonth{};
+  float sum{},
+  average{},
+  largest{},
+  smallest{},
+  largestMonth{},
+  smallestMonth{},
+  lastSmall{},
+  lastLarge{};
+
+  int month{0};
   vector <float> rainfall;
+
   while(month++ < 12) {
-    askForRain(rainfall, month, largestMonth, smallestMonth);
+    askForRain(rainfall,
+      month,
+      largestMonth,
+      smallestMonth,
+      lastSmall,
+      lastLarge
+    );
   }
   getResults(rainfall, sum, average, largest, smallest);
   printResults(sum, average, largest, smallest, largestMonth, smallestMonth);
