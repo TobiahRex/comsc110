2914bec8c587784d5718ec7ad74affab5a5fba43
Tobiah Rex <tobiah.rex@gmail.com>
2018-07-05 17:21:03 -0700

    tested and finished 6a.

--- AS6/6a-largest-sum.cpp
+++ AS6/6a-largest-sum.cpp
@@ -12,24 +12,24 @@
 
 using namespace std;
 
-void printResult(int & num1, int & num2) {
-  cout << "\n\nThe largest value entered is " << num1 << endl;
-  cout << "\n\nThe smallest value entered is " << num2 << endl;  
+void printResults(int & num1, int & num2) {
+  cout << "\n\nThe largest value entered is " << num2 << endl;
+  cout << "The smallest value entered is " << num1 << endl;
 }
 
-void getBigSmall(vector<int> & v) {
+void getResults(vector<int> & v) {
   sort(v.begin(), v.end());
 }
 
 void askForNumber(vector<int> & v) {
   string error("That is not a valid input.  Please enter a positive interger & try again");
   while(true) {
-    cout >> "Enter an interger value: ";
+    cout << "Enter an interger value: ";
     int number{};
     cin >> number;
 
     if (number == 0 || number < 0) {
-      cout << error;
+      cout << error << endl;
       cin.clear();
       cin.ignore();
     } else {
@@ -39,15 +39,19 @@ void askForNumber(vector<int> & v) {
   }
 }
 
-int main() {
-  string prompt("\nThis program will ask you to\n enter ten values, then it will\n determine the largest and smallest\n of the values you entered.\n");
+vector<int> beginProgram() {
+  string prompt("\nThis program will ask you to\nenter ten values, then it will\ndetermine the largest and smallest\nof the values you entered.\n");
   cout << prompt << endl;
 
   vector<int> numbers = {};
-  while(numbers.length() < 10) askForNumber(numbers);
+  while(numbers.size() < 10) askForNumber(numbers);
+  return numbers;
+}
 
-  getBigSmall(numbers);
-  printResult(numbers[0], numbers[numbers.length() - 1]);
+int main() {
+  vector<int> ints = beginProgram();
+  getResults(ints);
+  printResults(ints[0], ints[ints.size() - 1]);
 
   return 0;
 }
