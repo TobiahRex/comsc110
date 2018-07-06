70052f260ff1144718b081e3d968570dc4322816
Tobiah Rex <tobiah.rex@gmail.com>
2018-07-05 19:58:02 -0700

    added "month" counter to largest and smallest counts.

--- AS6/6b-rainfall-stats.cpp
+++ AS6/6b-rainfall-stats.cpp
@@ -14,17 +14,24 @@ using namespace std;
 
 void printResults(float const& s, float const& a, float const& l, float const& sm) {
   cout << setprecision(2) << fixed;
-  cout << "the total rainfall for the year is " << s << " inches." << endl;
+  cout << "\nthe total rainfall for the year is " << s << " inches." << endl;
   cout << "the average rainfall for the year is " << a << " inches." << endl;
-  cout << "the largest amount of rainfall was " << s << " inches in month" << "<CHANGE>." << endl;
-  cout << "the smallest amount of rainfall was " << s << " inches in month" << "<CHANGE>." << endl;
+  cout << "the largest amount of rainfall was " << l << " inches in month" << "<CHANGE>." << endl;
+  cout << "the smallest amount of rainfall was " << sm << " inches in month" << "<CHANGE>." << endl;
 }
 
-void askForRain(vector<float> & n, int & month) {
+void getResults(vector<float> & r, float & sum, float & avg, float & lrg, float & sm) {
+  sort(r.begin(), r.end());
+  for (auto rain : r) sum += rain;
+  avg = sum / r.size();
+  lrg = r[r.size() - 1];
+  sm = r[0];
+}
+
+void askForRain(vector<float> & n, int & month, float & lrgM, float & smM) {
   string error("\nThat is not a valid input.  \nPlease enter a valid floating point number & try again\n");
 
   while(true) {
-    int monthCount{};
     cout << "Enter the rainfall (in inches) for month #" << month  << ": ";
     float number{};
     cin >> number;
@@ -34,6 +41,8 @@ void askForRain(vector<float> & n, int & month) {
       cin.clear();
       cin.ignore();
     } else {
+      if (number > lrgM) lrgM = n;
+      else if (number < smM) smM = n;
       n.push_back(number);
       break;
     }
@@ -42,21 +51,13 @@ void askForRain(vector<float> & n, int & month) {
 
 void beginProgram() {
   float sum{}, average{}, largest{}, smallest{};
-  int month{0};
+  int month{0}, largestMonth{}, smallestMonth{};
   vector <float> rainfall;
   while(month++ < 12) {
-    askForRain(rainfall, month);
+    askForRain(rainfall, month, largestMonth, smallestMonth);
   }
   getResults(rainfall, sum, average, largest, smallest);
   printResults(sum, average, largest, smallest);
-}
-
-void getResults(vector<float> const& r, float & sum, float & avg, float & lrg, float & sm) {
-  sort(r.begin(), r.end());
-  for (rain : r) sum += rain;
-  avg = sum / r.size();
-  lrg = r[r.size() - 1];
-  sm = r[0];
 }
 
 int main() {
