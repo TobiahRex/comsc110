624488aef060f602072b433a16d6897942886367
Tobiah Rex <tobiah.rex@gmail.com>
2018-07-11 11:17:40 -0700

    wrote "getFood" for 6d. Writing "calcStats".

--- AS6/6d-monkey-business.cpp
+++ AS6/6d-monkey-business.cpp
@@ -10,41 +10,47 @@
 #include <vector>
 using namespace std;
 
-int getFood(
+int calcStats(vector< vector<float> >) {
+
+}
+
+void getFood(
   int & i,
   int & j,
-  vector<float> & foods,
+  vector< vector<int> > &foodChart,
 ) {
-  string error("\nThat is not a valid input. Please try again.\n");
+  string error("\nThat is not a valid input.\n");
+  string nonNeg("\nEnter a non-negative number.\n");
   float food{};
 
   while(true) {
     cout << "Enter the pounds eaten by monkey number " << (i + 1) << " on day" << (j + 1) << ": ";
     cin >> food;
-    if (food == 0 || food < 0) {
+    if (food == 0) {
       cout << error;
+      cin.clear();
+      cin.ignore();
+    } else if (food < 0) {
+      cout << nonNeg;
       cin.clear();
       cin.ignore();
     } else {
       break;
     }
   }
-
-  vector<vector<int>> array_2d(rows, vector<int>(cols, 0));
-
+  foodChart[i][j] = food;
 }
 
 int main() {
-  int rows{3}, cols{5};
-  vector<vector<int>> foodChart(rows, vector<int>(cols, 0));
+  int rows{3}, cols{7};
+  vector<vector<float>> foodChart(rows, vector<float>(cols, 0));
 
-  for (int i{0}; i < 3; i++) {
-    for (int j{0}; j < 5; j++) {
-
-      getFood(i, j, foodChart);
-
-    }
+  for (int i{0}; i < rows; i++) {
+    for (int j{0}; j < cols; j++) getFood(i, j, foodChart);
   }
+
+  calcStats(foodChart);
+  printResults(foodChart);
 
   return 0;
 }
