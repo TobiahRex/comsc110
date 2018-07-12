82e2703438d1cbd9de6ecdda2f42db34fdfbbef5
Tobiah Rex <tobiah.rex@gmail.com>
2018-07-12 11:35:48 -0700

    building 6f.

---
+++ AS6/6f-bubble-sort-enhancement.cpp
@@ -0,0 +1,28 @@
+///**********************************************************************************
+/// Description: Implementation of the classic Bubble Sort - Enhancement
+/// Author: Tobiah Bickley
+/// COMSC 110 Section 1035
+/// Date: July 3, 2018
+/// Status : Complete
+///***********************************************************************************
+#include <iostream>
+using namespace std;
+
+int main() {
+  vector<int> nums = {101,99,88,77,66,55,44,33,22,11};
+  int size = nums.size();
+
+  for (int i{1}; i < size; i++) {
+    int largest{};
+    int limit = (size - i);
+    for (int j{0}; j < limit; j++) {
+      if (j != limit) {
+        
+      } else {
+        // swap
+      }
+    }
+  }
+
+  return 0;
+}
