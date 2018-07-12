facd72d1489c9d5c9fa3d0b600f705df0ed7a0d4
Tobiah Rex <tobiah.rex@gmail.com>
2018-07-12 11:30:36 -0700

    updated and finished 6e.

--- AS6/6e-bubble-sort.cpp
+++ AS6/6e-bubble-sort.cpp
@@ -7,10 +7,29 @@
 ///***********************************************************************************
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 int main() {
   vector<int> nums = {101, 99, 88, 77, 66, 55, 44, 33, 22, 11};
+  int size = nums.size();
+
+  for(int j{0}; j < size; j++) {
+    int divide = 0;
+    for (int i{1}; i < size; i++) {
+      if (nums[divide] > nums[i]) {
+        int temp = nums[i];
+        nums[i] = nums[divide];
+        nums[divide] = temp;
+        divide += 1;
+      } 
+    }
+  }
+
+  for (int i{1}; i < size; i++) {}
+
+  cout << "\n";
+  for (auto num : nums) cout << " " << num;
 
   return 0;
 }
