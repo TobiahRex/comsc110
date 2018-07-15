be4eb23a68aa2ae3aa1429fef470b0d0070af71f
Tobiah Rex <tobiah.rex@gmail.com>
2018-07-15 14:46:57 -0700

    creating a pointer sandbox.

---
+++ AS7/7c-file-tail.cpp
@@ -0,0 +1,10 @@
+///**********************************************************************************
+/// Description: Display the tail end of a text file.
+/// Author: Tobiah Bickley
+/// COMSC 110 Section 1035
+/// Date: July 14, 2018
+/// Status : Complete
+///***********************************************************************************
+#include <iostream>
+#include <fstream>
+#include <string>
---
+++ Tests/pointer-practice.cpp
@@ -0,0 +1,11 @@
+///**********************************************************************************
+/// Description: Practicing pointer: "address-of" & "dereference operator"
+/// Author: Tobiah Bickley
+/// COMSC 110 Section 1035
+/// Date: July 14, 2018
+/// Status : Complete
+///***********************************************************************************
+
+int main() {
+
+}
