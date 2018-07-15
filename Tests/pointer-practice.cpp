ac29b03ff5ee2b03bfc22feb4aad9cc991036f40
Tobiah Rex <tobiah.rex@gmail.com>
2018-07-15 15:29:36 -0700

    building out another test-case for pointers.

--- Tests/pointer-practice.cpp
+++ Tests/pointer-practice.cpp
@@ -5,7 +5,39 @@
 /// Date: July 14, 2018
 /// Status : Complete
 ///***********************************************************************************
+#include <iostream>
+#include <string>
+using namespace std;
+
+void testOne();
+void testTwo();
 
 int main() {
+  string lineBreak(25, '=');
 
+  cout << lineBreak << endl;
+  testOne();
+  cout << lineBreak << endl;
+  testTwo();
+}
+
+void testOne() {
+  int myvar;
+
+  int * foo = &myvar;
+  cout << "foo: " << foo << endl; // hex address.
+
+  int bar = *foo;
+  cout << "bar: " << bar << endl; // 25
+}
+
+void testTwo() {
+  int firstValue, secondValue;
+  int * pointer;
+
+  pointer = &firstValue;
+  cout << "pointer: " << pointer << endl;
+
+  *pointer = 10;
+  cout << "firstValue: " << firstValue << endl;
 }
