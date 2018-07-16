92b5283b486e2f08bb27c4d8737faf64d819c05c
Tobiah Rex <tobiah.rex@gmail.com>
2018-07-15 18:24:48 -0700

    almost finished writing 7c.

--- AS7/7c-file-tail.cpp
+++ AS7/7c-file-tail.cpp
@@ -8,13 +8,30 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <regex>
+using namespace std;
 
-void getFileName();
+void getFileName(string &f);
+void readFile(string &f);
+vector<string> splice(vector<string> &arr, int start, int stop);
 
 int main() {
   string fileName;
   getFileName(fileName);
   return 0;
+}
+
+vector<string> splice(vector<string> &arr, int start, int stop) {
+  vector<string>::const_iterator right = arr.begin() + stop;
+  vector<string>::const_iterator left;
+
+  switch(start) {
+    case 0: left = arr.begin(); break;
+    default: left = arr.begin() + start;
+  }
+
+  vector<string> newArr(left, right);
+  return newArr;
 }
 
 void getFileName(string &f) {
@@ -28,5 +45,24 @@ void getFileName(string &f) {
       cin.clear();
       cin.ignore();
     } else break;
+  }
+}
+
+void readFile(string &f) {
+  ifstream infile("/Users/BICKLEY/code/cs/comsc110/AS7/" + fileName);
+  vector<string> lines;
+  string line, eofile("That is the whole file.");
+  int lineCount{};
+
+  while(getline(infile, line)) {
+    linesCount++;
+    lines.push_back(line);
+
+    if(infile.eof()) {
+      vector<string> last10 = splice(lines, (lines.size() - 10), lines.end());
+      for (auto line : lines) cout << line << endl;
+      if (linesCount < 10) cout << endl << eofile << endl;
+      break;
+    }
   }
 }
