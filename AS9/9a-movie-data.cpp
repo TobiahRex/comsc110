093a6569f7bd6f76501d23d6deb4b210bc27b68f
Tobiah Rex <tobiah.rex@gmail.com>
2018-07-18 11:22:57 -0700

    finished writing 9a.

--- AS9/9a-movie-data.cpp
+++ AS9/9a-movie-data.cpp
@@ -7,28 +7,37 @@
 ///***********************************************************************************
 #include <iostream>
 #include <vector>
-#include <strings>
+#include <string>
 using namespace std;
 
-void printResults(movie_data & movie) {
-  vector<string> outputFields = {"Title: ", "Director: ", "Released: ", "Running Time: "};
-  vector<string> structFields = {"title", "director", "released", "run_time"};
+struct movie_data {
+  string title;
+  string director;
+  string released;
+  string run_time;
+  int member_size = 4;
+} movie1, movie2;
 
-  for (int i = 0; i < movie.size(); i++) {
-    cout << outputFields[i] << movie[structFields[i]] << endl;
-  }
+void printResults(movie_data &movie) {
+    cout << "\nTitle: " << movie.title << endl;
+    cout << "Director: " << movie.director << endl;
+    cout << "Released: " << movie.released << endl;
+    cout << "Running Time: " << movie.run_time << endl;
 };
 
 int main() {
-  struct movie_data {
-    string title
-    string director
-    int released
-    int run_time
-    vector<int> size
-  } movie1, movie2;
+  movie1.title = "War of the Worlds";
+  movie1.director = "Byron Haskin";
+  movie1.released = "1953";
+  movie1.run_time = "88 minutes";
 
+  movie2.title = "War of the Worlds";
+  movie2.director = "Steven Spielberg";
+  movie2.released = "2005";
+  movie2.run_time = "118 minutes";
 
+  vector<movie_data> movies = {movie1, movie2};
+  for (int i = 0; i < 2; i++) printResults(movies[i]);
 
   return 0;
 }
