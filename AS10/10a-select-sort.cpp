a8a8ee5dbbe94c829b9ec8b1546096e430d56e04
Tobiah Rex <tobiah.rex@gmail.com>
2018-07-26 09:38:47 -0700

    finished draft for 10a.

--- AS10/10a-select-sort.cpp
+++ AS10/10a-select-sort.cpp
@@ -13,8 +13,57 @@
 #include <limits>
 using namespace std;
 
+vector<int> getArr();
+void printArr(vector<int> &nums);
+void swap(int &a, int &b);
+void selectionSort(bool &right, vector<int> &nums);
+
 int main() {
-  
+  vector<int> nums;
+  nums = getArr();
+  cout << "The random array: ";
+  printArr(nums);
+
+  nums = getArr();
+  selectSort(true, nums);
+  printArr(nums);
+
+  nums = getArr();
+  selectSort(false, nums);
+  printArr(nums);
 
   return 0;
 }
+
+vector<int> getArr() {
+  vector<int> nums = {3,6,4,7,8,1,2,0,9,5};
+  return nums;
+}
+
+void printArr(vector<int> &nums) {
+  for (auto n : nums) cout << " " << n;
+}
+
+void swap(int &a, int &b) {
+  int temp = a;
+  a = b;
+  b = temp;
+}
+
+void selectionSort(bool &right, vector<int> &nums) {
+  int size = size(nums);
+
+  if (right) {
+    for (int i{0}; i < size; i++) {
+      for (int j = i + 1; j < size; j++) {
+        if (nums[i] > nums[j]) swap(nums[i], nums[j]);
+      }
+    }
+  } else {
+    for (int i{size}; i > 0; i++) {
+      for (int j = i - 1; j > 0; j++) {
+        if (nums[i] < nums[j]) swap(nums[i], nums[j]);
+      }
+    }
+  }
+};
