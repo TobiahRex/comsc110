59fad5c87a34b7726a3fe4ca229571d31e730404
Tobiah Rex <tobiah.rex@gmail.com>
2018-07-26 10:42:16 -0700

    refactoring 10c.

--- AS10/10c-factorial.cpp
+++ AS10/10c-factorial.cpp
@@ -6,26 +6,45 @@
 /// Status : Complete
 ///***********************************************************************************
 #include <iostream>
+#include <string>
 #include <vector>
 using namespace std;
 
-void factorialIt(int &n) {
-  vector<int> nums = getPrintArr();
-
+int factorialIt(int &n) {
   int solution{n};
-  while(n--) {
-    solution *= n;
-  }
+  while(--n) solution *= n;
   return solution;
 }
 
+int factorialRe(int &solution, int n) {
+  if (n == 1) return solution;
+  solution *= --n;
+  return factorialRe(solution, n);
+}
+
 int main() {
-  int n = 4;
+  int n;
+  int saved;
+  while(true) {
+    string error("That is not a valid number. Try again.");
+    cout << "Enter a number to find its factorial: ";
+    cin >> n;
+    if (cin.bad() || cin.error()) {
+      cin.clear();
+    } else {
+      break;
+    }
+  }
+  saved = n;
   cout << "n == " << n << endl;
   cout << "iterative: ";
   int iterative = factorialIt(n);
-  // cout << "recursive: ";
-  // int recursive = factorialRe(n);
+  cout << iterative << endl;
+
+  n = saved;
+  cout << "recursive: ";
+  int recursive = factorialRe(n, n);
+  cout << recursive << endl;
 
   return 0;
 }
---
+++ AS10/10d-recursion.cpp
@@ -0,0 +1,16 @@
+///**********************************************************************************
+/// Description: Using recursion.
+/// Author: Tobiah Bickley
+/// COMSC 110 Section 1035
+/// Date: July 26, 2018
+/// Status : Complete
+///***********************************************************************************
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+  
+
+  return 0;
+}
